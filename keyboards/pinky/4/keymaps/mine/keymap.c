89cdb8852d7e4e8b10a5ea00e405a8303ce46c9c
zmil <zmilrod@gmail.com>
2024-02-04 19:46:09 -0500

    pinky keymap by memory

--- keyboards/pinky/4/keymaps/mine/keymap.c
+++ keyboards/pinky/4/keymaps/mine/keymap.c
@@ -33,23 +33,23 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_4x7_4(
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_LPRN,    KC_RPRN, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
-        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LBRC,    KC_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
-        CTL_TAB, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_LCBR,    KC_RCBR, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , ADJUST ,    ADJUST , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT ,
-                                   KC_LALT, LOWER  , KC_LGUI, KC_SPC ,    KC_SPC , KC_RGUI, RAISE  , KC_RALT
+        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_LPRN,    KC_RPRN, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV,
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LBRC,    KC_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
+        LOWER, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_LCBR,    KC_RCBR, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , ADJUST ,    ADJUST , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT ,
+                                   KC_LALT, KC_LGUI  , KC_LCTL, KC_SPC ,    KC_SPC , RAISE, KC_BSPC  , KC_RGUI
     ),
     [_LOWER] = LAYOUT_split_4x7_4(
-        KC_GRV , _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, KC_MINS, KC_EQL , _______,
+        KC_GRV , _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,KC_BSLS , KC_EQL , _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+        KC_CAPS, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______,    _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT_split_4x7_4(
         KC_GRV , _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, KC_MINS, KC_EQL , _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,    _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______,    _______, _______, _______, _______
     ),
