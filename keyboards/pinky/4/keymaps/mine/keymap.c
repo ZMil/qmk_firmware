14a74f0947339a871fd4c9ce781ce33d9edb1335
Zach Milrod <zmilrod@gmail.com>
2024-03-14 11:29:49 -0400

    updates

--- keyboards/pinky/4/keymaps/mine/keymap.c
+++ keyboards/pinky/4/keymaps/mine/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_LBRC,    KC_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS,
         LOWER, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_LCBR,    KC_RCBR, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , ADJUST ,    ADJUST , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT ,
-                                   KC_LALT, KC_LGUI  , KC_LCTL, KC_SPC ,    KC_SPC , RAISE, KC_BSPC  , KC_RGUI
+                                   KC_LALT, KC_LGUI  , KC_LCTL, KC_SPC ,    KC_ENT , RAISE, KC_BSPC  , KC_RGUI
     ),
     [_LOWER] = LAYOUT_split_4x7_4(
         KC_GRV , _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,KC_BSLS , KC_EQL , _______,
